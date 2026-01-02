590d49a8f59930462fc3be1db6b4d63f587903b3
Kawiarsa <kawi.arsa99@gmail.com>
2026-01-02 11:41:18 +0800

    feature: Completed the breadcrumb feature

--- src/app/components/Header/header.js
+++ src/app/components/Header/header.js
@@ -2,39 +2,63 @@
 
 import styles from "./header.module.css"
 import { usePathname } from "next/navigation"
+import Link from "next/link"
 import { capitalize } from "@/app/utils/util"
 
 function Breadcrumb() {
     const urlPath = usePathname()
-    const splitUrl = urlPath.split("/")
-    const navigations = [{
-        label: "Pokemon",
-        href: "/pokemon"
-    },
-    {
-        label: capitalize(splitUrl.findLast()),
-        href: `/${splitUrl.findLast()}`
-    }]
+
+    const segments = urlPath.split("/").filter(segment => segment !== "")
+
+    const navigations = [
+        // { label: "Home", href: "/" },
+        ...segments.map((segment, index) => {
+            const href = "/" + segments.slice(0, index + 1).join("/")
+            console.log(segment)
+            return {
+                label: capitalize(segment),
+                href: href
+            }
+        })
+    ]
+
+
+
+    if (segments.length === 0) {
+        return <div><p>Home</p></div>
+    }
 
     return (
         <div>
-            <p>{formatSlash}</p>
+            {navigations.map((nav, index) => {
+                const isLast = index === navigations.length - 1
+
+                return (
+                    <span key={nav.href}>
+                        {isLast ? (
+                            <span>{nav.label}</span>
+                        ) : (
+                            <>
+                                <Link href={nav.href}>{nav.label}</Link>
+                                <span> {`>`} </span>
+                            </>
+                        )}
+                    </span>
+                )
+            })}
         </div>
     )
 }
 
 export function Header() {
-
     return (
         <div>
-            <div className={styles.wrapper}>
-                <p>Test</p>
-                <p>Test 2</p>
-            </div>
-            <div>
-                <Breadcrumb />
-            </div>
 
+            <div className={styles.wrapper}>
+                <div className={styles.navigation}>
+                    <Breadcrumb />
+                </div>
+            </div>
         </div>
     )
 }
